1b501415da0d6c69074646cfb68cedbc3fb83729
vanis-27 <svani271416@gmail.com>
2025-05-04 07:05:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openCard() {
+    const card = document.querySelector('.card');
+    const music = document.getElementById('birthdayMusic');
+    card.classList.add('open');
+    music.play();
+  
+    // Optional: Add confetti animation trigger here if you have one
+  }
+  
